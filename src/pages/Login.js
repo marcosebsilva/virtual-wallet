23a9c77e28b42488957354b71830ef2f11cd2076
Marcos Estevao <dusdoom@gmail.com>
2021-09-04 18:06:19 -0300

    added css files without styles

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import loginAction from '../actions';
+import '../Login.css';
 
 class Login extends React.Component {
   constructor() {
@@ -33,30 +34,45 @@ class Login extends React.Component {
     const { isDisabled } = this.state;
     const { login } = this.props;
     return (
-      <div className="login-form">
-        <input
-          name="email"
-          type="text"
-          onChange={ this.handleChange }
-          placeholder="Insira o email"
-          data-testid="email-input"
-        />
-        <input
-          name="password"
-          type="text"
-          placeholder="Insira a senha"
-          data-testid="password-input"
-          onChange={ this.handleChange }
-        />
-        <Link to="/carteira">
-          <button
-            disabled={ isDisabled ? 'disabled' : undefined }
-            type="button"
-            onClick={ () => login(this.state) }
-          >
-            Entrar
-          </button>
-        </Link>
+      <div className="c-login">
+        <section className="c-login__form">
+          <h1>Login</h1>
+          <input
+            name="email"
+            type="text"
+            onChange={ this.handleChange }
+            placeholder="Insira o email"
+            data-testid="email-input"
+          />
+          <input
+            name="password"
+            type="text"
+            placeholder="Insira a senha"
+            data-testid="password-input"
+            onChange={ this.handleChange }
+          />
+          <label htmlFor="remember-me">
+            <input type="checkbox" name="remember-me" id="remember-me" />
+            Lembrar de mim
+          </label>
+          <Link to="/carteira">
+            <button
+              disabled={ isDisabled ? 'disabled' : undefined }
+              type="button"
+              onClick={ () => login(this.state) }
+            >
+              Entrar
+            </button>
+          </Link>
+          <Link to="/cadastro">
+            Cadastrar
+          </Link>
+        </section>
+        <section className="c-login__contact">
+          <div>GitHub</div>
+          <div>Linkedin</div>
+          <div>Portfolio</div>
+        </section>
       </div>
     );
   }
